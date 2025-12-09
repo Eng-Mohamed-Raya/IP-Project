483ff562dcef623a0507cea43fed331cff63ce80
Eng-Mohamed-Raya <mohamedyasserraya@gmail.com>
2025-12-09 21:52:14 +0200

    edit header & footer files

--- js/footer.js
+++ js/footer.js
@@ -1,58 +1,25 @@
 const footerContainer = document.createElement("div");
 footerContainer.className = "container";
 
-const footer = document.createElement("footer");
-
-
-const footerLinks = document.createElement("ul");
-footerLinks.className = "footer-links row";
-
-const links1 = [
-  { text: "privacy policy", href: "#" },
-  { text: "terms of service", href: "#" },
-  { text: "contact us", href: "#" }
-];
-
-links1.forEach(link => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-
-  a.textContent = link.text;
-  a.href = link.href;
-
-  li.appendChild(a);
-  footerLinks.appendChild(li);
-});
-
-
-const socialUl = document.createElement("ul");
-socialUl.className = "footer-social-media row";
-
-const socialIcons = [
-  "fa-square-facebook",
-  "fa-square-twitter",
-  "fa-square-instagram"
-];
-
-socialIcons.forEach(iconClass => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  const i = document.createElement("i");
-
-  i.className = `fa-brands ${iconClass}`;
-  a.appendChild(i);
-  li.appendChild(a);
-  socialUl.appendChild(li);
-});
-
-
-const p = document.createElement("p");
-p.innerHTML = "&copy; 2024 Coffee Shop. All rights reserved.";
-
-
-footer.appendChild(footerLinks);
-footer.appendChild(socialUl);
-footer.appendChild(p);
-
-footerContainer.appendChild(footer);
+footerContainer.innerHTML=`
+<footer>
+      <ul class="footer-links row">
+        <li><a href="#">privacy policy</a></li>
+        <li><a href="#">terms of service</a></li>
+        <li><a href="#">contact us</a></li>
+      </ul>
+      <ul class="footer-social-media row">
+        <li>
+          <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+        </li>
+      </ul>
+      <p>&copy; 2024 Coffee Shop. All rights reserved.</p>
+    </footer>
+`
 document.body.appendChild(footerContainer);
--- js/header.js
+++ js/header.js
@@ -2,48 +2,18 @@
 const container = document.createElement("div");
 container.className = "container";
 
-
-const header = document.createElement("header");
-
-
-const logo = document.createElement("div");
-logo.className = "logo";
-
-const h1 = document.createElement("h1");
-h1.textContent = "Coffee Shop";
-
-logo.appendChild(h1);
-
-
-const nav = document.createElement("nav");
-const ul = document.createElement("ul");
-
-
-const links = [
-  { text: "Home", href: "/" },
-  { text: "Menu", href: "./menu.html" },
-  { text: "About", href: "./about.html" },
-  { text: "Contact", href: "./contact.html" },
-  { text: "order now", href: "/order", class: "btn" }
-];
-
-
-links.forEach(link => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-
-  a.textContent = link.text;
-  a.href = link.href;
-
-  if (link.class) a.classList.add(link.class);
-
-  li.appendChild(a);
-  ul.appendChild(li);
-});
-
-
-nav.appendChild(ul);
-header.appendChild(logo);
-header.appendChild(nav);
-container.appendChild(header);
+container.innerHTML=`
+<header>
+      <div class="logo"><h1>Coffee Shop</h1></div>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="./menu.html">Menu</a></li>
+          <li><a href="./about.html">About</a></li>
+          <li><a href="./contact.html">Contact</a></li>
+          <li><a href="/order" class="btn">order now</a></li>
+        </ul>
+      </nav>
+    </header>
+`;
 document.body.appendChild(container);
